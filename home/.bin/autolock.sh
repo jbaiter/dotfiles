7774cd379ab63eaf1f977dd8947a93ac63d59bb6
Johannes Baiter <johannes.baiter@gmail.com>
2016-05-19 09:14:11 +0200

    Increase locking timeout

--- home/.bin/autolock.sh
+++ home/.bin/autolock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 exec xautolock -detectsleep \
-  -time 1 -locker "$HOME/.bin/lock.py" \
-  -notify 30 \
+  -time 5 -locker "$HOME/.bin/lock.py" \
+  -notify 35 \
   -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
